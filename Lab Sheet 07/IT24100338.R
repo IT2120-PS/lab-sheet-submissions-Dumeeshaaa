f7b8cf045af8ab06074caeb370769fc7aaa13c15
IT24100338 <IT24100338@my.sliit.lk>
2025-09-16 23:39:12 +0530

    Add files via upload

---
+++ Lab Sheet 07/IT24100338.R
@@ -0,0 +1,10 @@
+setwd("/Users/dumeesha/Desktop/IT24100338")
+
+
+punif(25, min = 0, max = 40) - punif(10, min = 0, max = 40)
+
+pexp(2, rate = 1/3)
+
+pnorm(130, mean = 100, sd = 15)
+
+qnorm(0.95, mean = 100, sd = 15)
